0974ff9d62bf79fd0b828f9c639faf0346ba0c7b
101175 <160013569+101175@users.noreply.github.com>
2025-04-01 12:07:20 +0000

    added requirements

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def delete_character(index):
     return jsonify({"error": "Character not found"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=0.0.0.0, port=10000)
