9827f1baca0629bf6ee317436bc55aaa6dafcca1
101175 <160013569+101175@users.noreply.github.com>
2025-04-01 12:16:57 +0000

    fixed host and port

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def delete_character(index):
     return jsonify({"error": "Character not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host=0.0.0.0, port=10000)
+    app.run(host="0.0.0.0", port=10000)
